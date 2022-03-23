e2b218d34cacc794a11f243038838ef25aa61fa7
Robert Klein <roklein@gmx.net>
2022-03-23 11:28:20 +0100

    Ertellung Diagramme

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -3,16 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %% Öffnen der Datei und konvertieren zu numpy-Array
-file_name =  'input_data/power_data_1.txt'
-power_data_watts = open(file_name).read().split("\n")
-x = np.array(power_data_watts)
+for i in range(3):
+    m = str(i + 1)
+    x = ('input_data/power_data_'+m +'.txt')
+    power_data_watts = open(x).read().split("\n")
+    y = np.array(power_data_watts)
+    plt.title("Line graph")
+    plt.plot(y, color="blue")
+    plt.show()
 
-# %% Erstellen des Plots
-plt.title("Line graph")
-plt.plot(x, color="red")
 
-plt.show()
+# %%
 
+# %%
 
 # %%
 
