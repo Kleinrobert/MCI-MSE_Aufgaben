730c74bd4f42d0dfb17ee64b81b0a28e0fc39a1e
Muigg10 <101864110+Muigg10@users.noreply.github.com>
2022-04-03 23:20:59 +0200

    commit

--- PÜ3/main_3_functional.py
+++ PÜ3/main_3_functional.py
@@ -1,64 +1,63 @@
 # UC 2.0
 
-#%% UC 2.1 Einlesen der Daten
-
-
-list_of_new_tests = []
-## Überprüfen ob Dateien vorhanden sind
-
+# Importieren der nötigen Packete
 import os
 import pandas as pd
+import neurokit2 as nk
+import json
 
-folder_current = os.path.dirname(__file__) 
-folder_input_data = os.path.join(folder_current, 'input_data')
-for file in os.listdir(folder_input_data):
+#%% UC 2.1 Einlesen der Daten
+## Überprüfen ob Dateien vorhanden sind
+def get_data():
     
-    if file.endswith(".csv"):
-        file_name = os.path.join(folder_input_data, file)
-        print(file_name)
-        subject_id = file_name.split(".")[0][-1]
-        new_ecg_data= pd.read_csv(file_name)
+    list_of_new_tests = []
+
+    folder_current = os.path.dirname(__file__) 
+    folder_input_data = os.path.join(folder_current, 'input_data')
+    for file in os.listdir(folder_input_data):
+    
+        if file.endswith(".csv"):
+            file_name = os.path.join(folder_input_data, file)
+            print(file_name)
+            subject_id = file_name.split(".")[0][-1]
+            new_ecg_data= pd.read_csv(file_name)
 ## Erstellen einer Liste von Tests, die zu verarbeiten sind
 
-        list_of_new_tests.append(new_ecg_data)
+            list_of_new_tests.append(new_ecg_data)
 
 
-new_ecg_data["Subject_3"].plot()
+            new_ecg_data["Subject_3"].plot()
 
 #%% UC 2.2 Vorverarbeiten der Daten
 
 ## Anlegen einer Zeitreihe der Herzfrequenz aus den EKG-Daten
-
-import neurokit2 as nk
-
-ekg_data=pd.DataFrame() #Erstellen einer Tabelle
-ekg_data["ECG"] = new_ecg_data["Subject_3"]
+def analyze_data():
+    ekg_data=pd.DataFrame() #Erstellen einer Tabelle
+    ekg_data["ECG"] = new_ecg_data["Subject_3"]
 
 # Find peaks
-peaks, info = nk.ecg_peaks(ekg_data["ECG"], sampling_rate=1000)
+    peaks, info = nk.ecg_peaks(ekg_data["ECG"], sampling_rate=1000)
 # Herzschlag messen
-number_of_heartbeats = peaks["ECG_R_Peaks"].sum()
+    number_of_heartbeats = peaks["ECG_R_Peaks"].sum()
 # Min Dauer
-duration_test_min = ekg_data.size/1000/60
+    duration_test_min = ekg_data.size/1000/60
 # Durschnittliche Herzfrequenz
-average_hr_test = number_of_heartbeats / duration_test_min
+    average_hr_test = number_of_heartbeats / duration_test_min
 
 ## Calculate heart rate moving average
 
-peaks['average_HR_10s'] = peaks.rolling(window=10000).mean()*60*1000
-peaks['average_HR_10s'].plot()
+    peaks['average_HR_10s'] = peaks.rolling(window=10000).mean()*60*1000
+    peaks['average_HR_10s'].plot()
 
 
 #%% UC 2.3 Analysieren der Daten auf Abbruch-Kriterium
-
+#Wird nicht im vorhinein abgebrochen
 termination = False
-
 
 ## Vergleich der Maximalen Herzfrequenz mit Alter des Patienten
 
 folder_input_data = os.path.join(folder_current, 'input_data')
 
-import json
 # Opening JSON file
 
 file_name = folder_input_data = os.path.join(folder_input_data, 'subject_3.json')
@@ -79,13 +78,15 @@ if maximum_hr > subject_max_hr*0.90:
 
 #%% UC 2.4 Erstellen einer Zusammenfassung
 
-print("Summary for Subject " + str(subject_data["subject_id"]))
-print("Year of birth:  " + str(subject_data["birth_year"]))
-print("Test level power in W:  " + str(subject_data["test_power_w"]))
-print(" \n")
-print("Maximum HR was: " + str(maximum_hr))
-print("Was test terminated because exceeding HR " + str(termination))
+def print_summary():
+    print("Summary for Subject " + str(subject_data["subject_id"]))
+    print("Year of birth:  " + str(subject_data["birth_year"]))
+    print("Test level power in W:  " + str(subject_data["test_power_w"]))
+    print(" \n")
+    print("Maximum HR was: " + str(maximum_hr))
+    print("Was test terminated because exceeding HR " + str(termination))
 
+print_summary()
 ## Ausgabe einer Zusammenfassung
 
 #%% UC 2.5 Visualisierung der Daten
@@ -131,7 +132,6 @@ if manual_termination != False:
 
 
 #%% UC 2.7 Speichern der Daten
-
 
 # Speichern der Daten
 data = {"User ID": subject_data["subject_id"], "Reason for test termation": manual_termination, "Average Heart Rate": average_hr_test, "Maximum Heart Rate": subject_max_hr, "Test Length (s)": len(power_data_watts), "Test Power (W)": subject_data["test_power_w"], "Average Power": peaks_downsampled["Power (Watt)"].mean()}
